02a628db129a3a31fd580e6588b16a659fd8d81a
Juan Cuevas <juanandrescuevas14@gmail.com>
2023-03-19 15:43:36 -0400

    Repartir cartas

--- Blackjack.java
+++ Blackjack.java
@@ -6,10 +6,11 @@ public class Blackjack{
         boolean continuar = true;
         int juegosMax = 5;
         int apuestaMin = 10;
-        int[] baraja = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-                        17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
-                        33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,
-                        49,50,51,52,53,54,55,56};
+        int[] baraja = {
+            0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,
+            28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,
+            53,54,55
+        };
         int[] manoJugador;
         int[] manoCroupier;
         int[] resultado;
@@ -35,9 +36,13 @@ public class Blackjack{
             System.out.println("Apuesta: " + apuesta);
             creditos -= apuesta;
             System.out.println("Sus creditos restantes son: " + creditos);
+            manoJugador = RepartirCartasIniciales.repartirCartas(baraja);
+            System.out.println("Sus cartas son: " + manoJugador[0] + " y " + manoJugador[1]);
+            manoCroupier = RepartirCartasIniciales.repartirCartas(baraja);
+            System.out.println("Las cartas del croupier son: " + manoCroupier[0] + " y " + manoCroupier[1]);
             juegos++;
         }
-        
+        lectura.close();
         System.out.println("Juegos jugados: " + juegos + " de " + juegosMax);
         System.out.println("Juegos ganados: " + ganadas);
         System.out.println("Juegos perdidos: " + perdidas);
